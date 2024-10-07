d86e8f829dd0b5bf719959c658b082350eab2326
Jaskaran Singh <jaskaransingh@Jaskarans-Laptop.local>
2024-10-07 18:05:40 +0530

    V1 release

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 import "./interfaces/ITRC20.sol";
+import "./PriceOracle.sol";
 
 /**
  * @title LiquidityPool.
@@ -19,23 +20,33 @@ contract LiquidityPool {
         address investor;
         uint256 balanceTRX;
         uint256 balanceJST;
+        uint256 lastInvestedTRXTimestamp;
+        uint256 lastInvestedJSTTimestamp;
+        uint256 borrowedTRX;
+        uint256 borrowedJST;
     }
-
     event NewInvestor(address investor);
     event JSTAdded(address investor, uint256 amountJST);
     event TRXAdded(address investor, uint256 amountTRX);
     event FlashLoanTRXWithdrawn(uint256 amountTRX);
     event FlashLoanJSTWithdrawn(uint256 amountJST);
+    event TRXWithdrawn(address investor, uint256 amountTRX);
+    event JSTWithdrawn(address investor, uint256 amountJST);
+    event BorrowedTRX(address borrower, uint256 amountTRX);
+    event BorrowedJST(address borrower, uint256 amountJST);
+    event RepiadTRX(address borrower, uint256 amountRepaidWithIntrest);
 
     address public RAPID_LOANS_CORE;
     address public JST_CONTRACT_ADDRESS_NILE =
         0x37349aEB75a32f8c4c090DAFF376cF975F5d2eBA;
     ITRC20 public jst = ITRC20(JST_CONTRACT_ADDRESS_NILE);
+    PriceOracle public priceOracle;
 
     /**
      * @notice Borrow rate for liquidity pool loans determined by governance, NOT FLASH LOANS.
      */
-    uint256 public BORROW_RATE = 6;
+    uint256 public PROFIT_RATE = 6;
+    uint256 public BORROW_RATE = 7;
     uint256 public profitFromFlashLoansTRX;
     uint256 public profitFromFlashLoansJST;
     /**
@@ -51,13 +62,18 @@ contract LiquidityPool {
      */
     mapping(address => uint256) internal investorIndexes;
 
-    constructor() {
+    constructor(address _priceOracle) {
+        priceOracle = PriceOracle(_priceOracle);
         investorIdCounter = 1;
         investor memory initialInvestor = investor({
             investorId: 0,
             investor: address(0),
             balanceTRX: 0,
-            balanceJST: 0
+            balanceJST: 0,
+            lastInvestedTRXTimestamp: block.timestamp,
+            lastInvestedJSTTimestamp: block.timestamp,
+            borrowedTRX: 0,
+            borrowedJST: 0
         });
         investors.push(initialInvestor);
     }
@@ -72,7 +88,7 @@ contract LiquidityPool {
 
     /**
      * @notice Function to add TRX to the liquidity pool, as an investment to earn APY.
-     * @dev Creates a new vault if new user or updates the balance.
+     * @dev Creates a new vault if new user or updates the balance, sets block.timestamp to current.
      * @return balanceTRX Amount added.
      */
     function addTRX() external payable returns (uint256 balanceTRX) {
@@ -82,7 +98,11 @@ contract LiquidityPool {
                 investorId: investorIdCounter,
                 investor: msg.sender,
                 balanceTRX: msg.value,
-                balanceJST: 0
+                balanceJST: 0,
+                lastInvestedTRXTimestamp: block.timestamp,
+                lastInvestedJSTTimestamp: block.timestamp,
+                borrowedTRX: 0,
+                borrowedJST: 0
             });
             investors.push(temp);
             investorIndexes[msg.sender] = investorIdCounter;
@@ -98,7 +118,7 @@ contract LiquidityPool {
     /**
      * @notice Function to add JST to the liquidity pool, as an investment to earn APY.
      * @notice You need to approve JST, this contract as the spender, amount same as param.
-     * @dev Creates a new vault if new user or updates the balance.
+     * @dev Creates a new vault if new user or updates the balance, sets block.timestamp to current.
      * @param amount Amount to invest.
      * @return balanceJST Amount added.
      */
@@ -114,7 +134,11 @@ contract LiquidityPool {
                 investorId: investorIdCounter,
                 investor: msg.sender,
                 balanceTRX: 0,
-                balanceJST: amount
+                balanceJST: amount,
+                lastInvestedTRXTimestamp: block.timestamp,
+                lastInvestedJSTTimestamp: block.timestamp,
+                borrowedTRX: 0,
+                borrowedJST: 0
             });
             investors.push(temp);
             investorIndexes[msg.sender] = investorIdCounter;
@@ -127,13 +151,121 @@ contract LiquidityPool {
         return investors[investorIndexes[msg.sender]].balanceJST;
     }
 
-    function withdrawTRX(uint256 amount) external {}
+    /**
+     * @notice Withdraw TRX from the liquidity pool, including intrest for one month.
+     * @notice Investment must be made 15 days prior to withdrawal.
+     * @param amount Amount of TRX to withdraw.
+     */
+    function withdrawTRX(
+        uint256 amount
+    ) external returns (uint256 amountWithdrawnTRX) {
+        require(amount > 0, "Invalid amount");
+        require(
+            investors[investorIndexes[msg.sender]].balanceTRX >= amount,
+            "Insufficient TRX balance"
+        );
+        require(
+            amount < investors[investorIndexes[msg.sender]].balanceTRX,
+            "Enter valid amount"
+        );
+        require(
+            investors[investorIndexes[msg.sender]].lastInvestedTRXTimestamp >
+                15 days,
+            "Amount is locked for 15 days"
+        );
+        investors[investorIndexes[msg.sender]].balanceTRX -= amount;
+        uint256 finalAmount = ((PROFIT_RATE * amount) / 100) + (amount);
+        payable(msg.sender).transfer(finalAmount);
+        emit TRXWithdrawn(msg.sender, amount);
+        return amount;
+    }
 
-    function withdrawJST(uint256 amount) external {}
+    /**
+     * @notice Withdraw JST from the liquidity pool, including intrest for one month.
+     * @notice Investment must be made 15 days prior to withdrawal.
+     * @param amount Amount of JST to withdraw.
+     */
+    function withdrawJST(
+        uint256 amount
+    ) external returns (uint256 amountWithdrawnJST) {
+        require(amount > 0, "Invalid amount");
+        require(
+            investors[investorIndexes[msg.sender]].balanceJST >= amount,
+            "Insufficient JST balance"
+        );
+        require(
+            amount < investors[investorIndexes[msg.sender]].balanceJST,
+            "Enter valid amount"
+        );
+        require(
+            investors[investorIndexes[msg.sender]].lastInvestedJSTTimestamp >
+                15 days,
+            "Amount is locked for 15 days"
+        );
+        investors[investorIndexes[msg.sender]].balanceJST -= amount;
+        uint256 finalAmount = ((PROFIT_RATE * amount) / 100) + (amount);
+        bool success = jst.transferFrom(address(this), msg.sender, finalAmount);
+        require(success, "Transfer of JST failed");
+        emit JSTWithdrawn(msg.sender, amount);
+        return amount;
+    }
 
-    function borrowTRX(uint256 amount) external {}
+    /**
+     * @notice Borrow TRX from the liquidity pool.
+     * @notice You must have equal JST tokens invested to borrow TRX.
+     * @notice While repaying the loan, you are charged intrest of one month, this is a one month only loan.
+     * @param amount Amount of TRX to borrow.
+     */
+    function borrowTRX(uint256 amount) external {
+        require(amount > 0, "Invalid amount");
+        require(
+            investors[investorIndexes[msg.sender]].balanceJST >=
+                priceOracle.getTRXToJST(amount),
+            "Insufficient JST balance"
+        );
+        uint256 amountToBeDeducted = priceOracle.getTRXToJST(amount);
+        investors[investorIndexes[msg.sender]].balanceJST -= amountToBeDeducted;
+        bool success = payable(msg.sender).send(amount);
+        require(success, "Transfer of TRX failed");
+        emit BorrowedTRX(msg.sender, amount);
+    }
 
-    function borrowJST(uint256 amount) external {}
+    /**
+     * @notice Repay TRX debt to liquidity pool.
+     * @notice You must have an outstanding debt.
+     * @notice Msg.value must be greater than principal + intrest.
+     */
+    function repayTRX() external payable {
+        require(msg.value > 0, "Invalid amount");
+        uint256 borrowedAmount = investors[investorIndexes[msg.sender]]
+            .borrowedTRX;
+        uint256 finalAmount = ((BORROW_RATE * borrowedAmount) / 100) +
+            (borrowedAmount);
+        require(msg.value >= finalAmount, "Repay the whole loan.");
+        investors[investorIndexes[msg.sender]].balanceTRX -= msg.value;
+        emit RepiadTRX(msg.sender, msg.value);
+    }
+
+    /**
+     * @notice Borrow JST from the liquidity pool.
+     * @notice You must have equal TRX tokens invested to borrow JST.
+     * @notice While repaying the loan, you are charged intrest of one month, this is a one month only loan.
+     * @param amount Amount of JST to borrow.
+     */
+    function borrowJST(uint256 amount) external {
+        require(amount > 0, "Invalid amount");
+        require(
+            investors[investorIndexes[msg.sender]].balanceTRX >=
+                priceOracle.getJSTToTRX(amount),
+            "Insufficient TRX balance"
+        );
+        uint256 amountToBeDeducted = priceOracle.getJSTToTRX(amount);
+        investors[investorIndexes[msg.sender]].balanceTRX -= amountToBeDeducted;
+        investors[investorIndexes[msg.sender]].balanceJST -= amount;
+        bool success = jst.transfer(msg.sender, amount);
+        require(success, "Transfer of JST failed");
+        emit BorrowedJST(msg.sender, amount);
+    }
 
     /**
      * @notice Withdraw TRX from the liquidity pool for a flash loan.
@@ -220,6 +352,30 @@ contract LiquidityPool {
         address investorAddress
     ) public view returns (uint256 amountJST) {
         return investors[investorIndexes[investorAddress]].balanceJST;
+    }
+
+    /**
+     * @param investorAddress Address of the investor.
+     * @return timestamp The timestamp of the last time the investor invested TRX.
+     */
+    function getUserLastInvestedTRXTimestamp(
+        address investorAddress
+    ) public view returns (uint256 timestamp) {
+        return
+            investors[investorIndexes[investorAddress]]
+                .lastInvestedTRXTimestamp;
+    }
+
+    /**
+     * @param investorAddress Address of the investor.
+     * @return timestamp The timestamp of the last time the investor invested JST.
+     */
+    function getUseLastInvestedJSTTimestamp(
+        address investorAddress
+    ) public view returns (uint256 timestamp) {
+        return
+            investors[investorIndexes[investorAddress]]
+                .lastInvestedJSTTimestamp;
     }
 
     /**
---
+++ contracts/PriceOracle.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title Oracle
+ * @author RapidLoans
+ * @notice A decentralized price oracle for RapidLoans, fetching price of TRX to JST and JST to TRX.
+ * @dev Admins can update prices, and users can fetch the latest prices.
+ */
+contract PriceOracle {
+    address public admin;
+    address public TRX = address(1);
+    address public JST = address(2);
+
+    mapping(address => uint256) public assetPrices;
+
+    event PriceUpdated(address indexed asset, uint256 newPrice);
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Caller is not the admin");
+        _;
+    }
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    /**
+     * @dev Function to set the price of an TRX to JST.
+     * @param price The new price in JST (18 decimals).
+     */
+    function setTRXToJST(uint256 price) external onlyAdmin {
+        require(price > 0, "Price must be greater than zero");
+        assetPrices[TRX] = price;
+        emit PriceUpdated(TRX, price);
+    }
+
+    /**
+     * @dev Function to set the price of an JST to TRX.
+     * @param price The new price in TRX (18 decimals).
+     */
+    function setJSTToTRX(uint256 price) external onlyAdmin {
+        require(price > 0, "Price must be greater than zero");
+        assetPrices[JST] = price;
+        emit PriceUpdated(JST, price);
+    }
+
+    /**
+     * @dev Function to get the latest price of TRX in JST.
+     * @param amount of TRX to convert to JST.
+     * @return The price of corrosponding TRX asset in JST (18 decimals).
+     */
+    function getTRXToJST(uint256 amount) external view returns (uint256) {
+        uint256 price = assetPrices[TRX] * amount;
+        require(price > 0, "Price not available for the given asset");
+        return price;
+    }
+
+    /**
+     * @notice Function to get the latest price of JST in TRX.
+     * @param amount of JST to convert to TRX.
+     * @return The price of corrosponding JST asset in TRX (18 decimals).
+     */
+    function getJSTToTRX(uint256 amount) external view returns (uint256) {
+        uint256 price = assetPrices[JST] * amount;
+        require(price > 0, "Price not available for the given asset");
+        return price;
+    }
+
+    /**
+     * @dev Function to change the admin of the contract.
+     * @param newAdmin The address of the new admin.
+     */
+    function changeAdmin(address newAdmin) external onlyAdmin {
+        require(newAdmin != address(0), "Invalid new admin address");
+        admin = newAdmin;
+    }
+}
